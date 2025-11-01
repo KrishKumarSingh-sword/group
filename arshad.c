cf8a25f8341c40dabdcb9b33fcabcb467a0bca2f
Krish Kumar Singh <krishkumarguns@gmail.com>
2025-11-01 07:20:33 +0530

    Add files via upload

---
+++ arshad.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+   int n;
+   printf("entre the range 10 to n");
+   scanf("%d", &n);
+   for(int j=10;j<n;j++)
+{   
+   int b=j;
+   int a=b;
+   int d;
+   int prod=1;
+   int sum=0;
+
+do
+{
+   d=a%10;
+   sum=sum+d;
+   a=a/10;
+}
+while(a!=0);
+
+
+printf("%d", j);
+if((b%sum)==0)
+{
+   printf("arshad");
+}
+else
+{
+   printf("not arshad");
+}
+printf("\n");
+sum=0;
+prod=1;
+
+}
+}
---
+++ strongnumber.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+   int n;
+   printf("entre the range 1 to n");
+   scanf("%d", &n);
+   for(int j=1;j<n;j++)
+{   
+   int b=j;
+   int a=b;
+   int d;
+   int prod=1;
+   int sum=0;
+
+do
+{
+   
+   d=a%10;
+   for(int i=1;i<=d;i++)
+   {
+      prod=prod*i;
+   }
+   sum=sum+prod;
+   prod=1;
+   a=a/10;
+}
+while(a!=0);
+
+if(sum==b)
+{
+   printf("strong");
+}
+else
+{
+   printf("not strong");
+}
+printf("\n");
+sum=0;
+prod=1;
+
+}
+}
